a96114d4135c1390fbebacd67ffbf3769d975971
Iván Morett <luisivanmorett@gmail.com>
2019-09-16 12:37:32 -0500

    Console project that tells if a DFA is equivalent to other

---
+++ AutomataEquivalence/Matrix.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutomataEquivalence
+{
+    class Matrix
+    {
+        private static int POZO = -1;
+        private readonly int[][] matrix;
+        private readonly List<int> start;
+        private readonly List<int> end;
+        private string[] _transitions;
+
+        private Matrix()
+        {
+            start = new List<int>();
+            end = new List<int>();
+        }
+        public Matrix(string[] lines)
+        {
+            if (lines.Length <= 0) return;
+            matrix = new int[lines.Length][];
+            start = new List<int>();
+            end = new List<int>();
+            _transitions = lines[0].Split(',');
+            for (var i = 1; i < lines.Length; i++)
+            {
+                var values = lines[i].Split(',');
+                matrix[i-1] = new int[values.Length];
+                for (var j = 0; j < values.Length; j++)
+                {
+                    var index = 0;
+                    if (j == 0 && values[j].Length > 1)
+                    {
+                        if (values[j][0] == '>' || (values[j].Length > 1 && values[j][1] == '>'))
+                        {
+                            start.Add(i-1);
+                            index++;
+                        }
+                        else if (values[j][0] == '*' || (values[j].Length > 1 && values[j][1] == '*'))
+                        {
+                            end.Add(i-1);
+                            index++;
+                        }
+                    }
+                    matrix[i - 1][j] = values[j].Substring(index).Length > 0 ? int.Parse(values[j].Substring(index)) : POZO;
+                }
+            }
+
+        }
+
+        public int Height()
+        {
+            return this.matrix.Length;
+        }
+
+        public int Width()
+        {
+            return this._transitions.Length;
+        }
+
+        public bool CompareSize(Matrix m)
+        {
+            return this.Height() == m.Height() && this.Width() == m.Width();
+        }
+
+        public int[] this[int i] => this.matrix[i];
+
+        public StateType GetStateType(int node)
+        {
+            bool start = this.start.Contains(node);
+            bool end = this.end.Contains(node);
+            if (start && end)
+                return StateType.StartFinal;
+            if (start)
+                return StateType.Start;
+            if (end)
+                return StateType.Final;
+            return StateType.Intermediate;
+        }
+
+        public int Start()
+        {
+            return this.start[0];
+        }
+
+        public string[] Transitions => this._transitions;
+
+        public int GetTransitionPosition(string transition)
+        {
+            for (var i = 0; i < _transitions.Length; i++)
+            {
+                if (_transitions[i] == transition)
+                    return i;
+            }
+
+            return -1;
+        }
+    }
+
+    enum StateType
+    {
+        Start = 0, Final = 1, Intermediate = 2, StartFinal = 3
+    }
+}
---
+++ AutomataEquivalence/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace AutomataEquivalence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Please enter the 2 matrix");
+                return;
+            }
+
+            var data1 = ReadFile(args[0]);
+            var data2 = ReadFile(args[1]);
+            if (data1==null || data2==null)
+            {
+                Console.ReadKey();
+                return;
+            }
+            var m1 = new Matrix(ReadFile(args[0]));
+            var m2 = new Matrix(ReadFile(args[1]));
+
+            Compare(m1, m2);
+            
+            Console.ReadKey();
+        }
+
+        private static void Compare(Matrix m1, Matrix m2)
+        {
+            if (!m1.CompareSize(m2))
+            {
+                Console.WriteLine("Not equivalent");
+                return;
+            }
+
+            List<Tuple<int, int>> visited = new List<Tuple<int, int>>();
+            Queue<Tuple<int, int>> toVisit = new Queue<Tuple<int, int>>();
+            toVisit.Enqueue(Tuple.Create(m1.Start(), m2.Start()));
+            while (toVisit.Count > 0)
+            {
+                var current = toVisit.Dequeue();
+                visited.Add(current);
+                foreach (var transition in m1.Transitions)
+                {
+                    var col1 = m1.GetTransitionPosition(transition);
+                    var col2 = m2.GetTransitionPosition(transition);
+                    if (col1 == -1 || col2 == -1)
+                    {
+                        Console.WriteLine("Not equivalent");
+                        return;
+                    }
+
+                    var tuple = Tuple.Create(m1[current.Item1][col1], m2[current.Item2][col2]);
+                    var stateM1 = m1.GetStateType(tuple.Item1);
+                    var stateM2 = m2.GetStateType(tuple.Item2);
+                    if (!(
+                        stateM1 == stateM2 ||
+                        stateM1 == StateType.StartFinal && (stateM2 == StateType.Start || stateM2 == StateType.Start) ||
+                        stateM2 == StateType.StartFinal && (stateM1 == StateType.Start || stateM1 == StateType.Start)
+                    ))
+                    {
+                        Console.WriteLine("Not equivalent");
+                        return;
+                    }
+
+                    if (!visited.Contains(tuple) && !toVisit.Contains(tuple))
+                    {
+                        toVisit.Enqueue(tuple);
+                    }
+                }
+            }
+            Console.WriteLine("Equivalent");
+        }
+
+        private static string[] ReadFile(string path)
+        {
+            try
+            {
+                return System.IO.File.ReadAllLines(path);
+            }
+            catch (DirectoryNotFoundException ex)
+            {
+                Console.WriteLine("Directory no found");
+                return null;
+            }
+        }
+    }
+}
